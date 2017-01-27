f1755b094e50c643e93bc5efbcd65e1b78134b6b
François Moisan <fmoisan@jive.com>
2017-01-27 16:20:45 -0500

    use remote host family as hint (if applicable) when opening connections

--- src/socket.cpp
+++ src/socket.cpp
@@ -2496,6 +2496,7 @@ void SIPpSocket::close_calls()
 int open_connections()
 {
     int status=0;
+    int family_hint = PF_UNSPEC;
     local_port = 0;
 
     if (!strlen(remote_host)) {
@@ -2521,6 +2522,7 @@ int open_connections()
             }
 
             get_inet_address(&remote_sockaddr, remote_ip, sizeof(remote_ip));
+            family_hint = remote_sockaddr.ss_family;
             if (remote_sockaddr.ss_family == AF_INET) {
                 strcpy(remote_ip_escaped, remote_ip);
             } else {
@@ -2547,7 +2549,7 @@ int open_connections()
 
         memset((char*)&hints, 0, sizeof(hints));
         hints.ai_flags  = AI_PASSIVE;
-        hints.ai_family = PF_UNSPEC;
+        hints.ai_family = family_hint;
 
         /* Resolving local IP */
         if (getaddrinfo(local_host, NULL, &hints, &local_addr) != 0) {
